d08f623fd0324eec1a9636524723858627f34a90
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2023-11-15 02:57:04 +0500

    added undo option

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TicTacToe {
     Scanner sc = new Scanner(System.in);
     final char[][] possibilities = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
-    char[] playerPicks = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char[] playerPicks = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    ArrayList<Integer> previousTurn = new ArrayList<>();
     int counter = 0;
     char XorO = 'X';
     String p1;
     String p2;
     boolean isGameEnd = false;
+    boolean isContinue = false;
 
     TicTacToe() {
         this.p1 = p1Name();
@@ -18,30 +21,63 @@ public class TicTacToe {
 
     void gameStart() {
         for (int i = 0; i < playerPicks.length; i++) {
+            isContinue = false;
             int XorO_pick = playerTurn();
+            while (XorO_pick == -1 && isGameEmpty()) {
+                System.out.println("There is no value undo.");
+                XorO_pick = playerTurn();
+                i = -1;
+                XorO = 'X';
+            }
+            if (XorO_pick == -1 && !isGameEmpty()) {
+                undoMove();
+                i -= 1;
+                continue;
+            }
             while (playerPicks[XorO_pick - 1] == 'X' || playerPicks[XorO_pick - 1] == 'O') {
                 System.out.println(XorO_pick + " is already taken by " + playerPicks[XorO_pick - 1]);
                 XorO_pick = playerTurn();
+                if (XorO_pick == -1) {
+                    undoMove();
+                    i -= 1;
+                    isContinue = true;
+                    break;
+                }
             }
+            if (isContinue) continue;
+            previousTurn.add(XorO_pick - 1);
             playerPicks[XorO_pick - 1] = XorO;
             counter++;
-            if (counter >= 5) {
-                checkPossibilities();
-            }
+            if (counter >= 5) checkPossibilities();
             if (isGameEnd) {
                 gameEnd();
                 return;
             }
-            ;
             XorO = XorO == 'X' ? 'O' : 'X';
         }
-        System.out.println("Player 1 (X): " + p1 + " & Player 2 (O): " + p2 + "\nGood luck you both!");
+        showGame();
+        System.out.println("Game drawn.");
+    }
+
+    void undoMove() {
+        playerPicks[previousTurn.get(previousTurn.size() - 1)] = ' ';
+        previousTurn.remove(previousTurn.size() - 1);
+        XorO = XorO == 'X' ? 'O' : 'X';
+    }
+
+    boolean isGameEmpty() {
+        for (char playerPick : playerPicks) {
+            if (playerPick != ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 
     void gameEnd() {
         showGame();
         String winner = XorO == 'X' ? p1 : p2;
-        System.out.println("Congratulations! " + winner + "'s have won! Thanks for playing.");
+        System.out.println("Congratulations! " + winner + " have won! Thanks for playing.");
     }
 
     String p1Name() {
@@ -66,9 +102,15 @@ public class TicTacToe {
 
     int playerTurn() {
         showGame();
-        System.out.println("Pick a number.");
-        System.out.print(XorO + " turn: ");
-        return sc.nextInt();
+        System.out.println("Pick a number between 1-9");
+        System.out.print("Enter " + XorO + " turn or type (-1) if you want to undo a move : ");
+        int guess = sc.nextInt();
+        while ((guess > 9 || guess < 1) && guess != -1) {
+            System.out.println("Please select numbers between 1-9");
+            System.out.print("Enter " + XorO + " turn or type (-1) if you want to undo a move : ");
+            guess = sc.nextInt();
+        }
+        return guess;
     }
 
     void checkPossibilities() {
@@ -85,12 +127,10 @@ public class TicTacToe {
     }
 
     void showGame() {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + playerPicks[0] + " | " + playerPicks[1] + " | " + playerPicks[2] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + playerPicks[3] + " | " + playerPicks[4] + " | " + playerPicks[5] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + playerPicks[6] + " | " + playerPicks[7] + " | " + playerPicks[8] + " |");
-        System.out.println("|---|---|---|");
+        System.out.println(" " + playerPicks[0] + " | " + playerPicks[1] + " | " + playerPicks[2]);
+        System.out.println("---+---+---");
+        System.out.println(" " + playerPicks[3] + " | " + playerPicks[4] + " | " + playerPicks[5]);
+        System.out.println("---+---+---");
+        System.out.println(" " + playerPicks[6] + " | " + playerPicks[7] + " | " + playerPicks[8]);
     }
 }
